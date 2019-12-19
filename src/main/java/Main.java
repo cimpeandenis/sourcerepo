051ba8b0e676372225c314e04938b352f2252bc2
Denis <cimpeandenis@gmail.com>
2019-12-19 11:36:32 +0200

    third

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    private static final String versionHash = "058fdb4b0c";
+    private static final String versionHash = "00b9d25391";
     private int l = 0;
+    private int d = 0;
     public static void main(String[] args) {
         System.out.println("NEW " + versionHash);
     }
