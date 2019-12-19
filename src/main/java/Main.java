00b9d253913a392106ee6ee469012860f74bae51
Denis <cimpeandenis@gmail.com>
2019-12-19 11:32:24 +0200

    first second2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    private static final String versionHash = "null";
+    private static final String versionHash = "058fdb4b0c";
+    private int l = 0;
     public static void main(String[] args) {
         System.out.println("NEW " + versionHash);
     }
